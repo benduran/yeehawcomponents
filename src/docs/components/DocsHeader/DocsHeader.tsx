ce08f275486c592102a69bebaef875bb6570def4
benduran <stratodyne@gmail.com>
2024-08-10 17:11:34 +0100

    chore: added some fonts for the docsite

---
+++ src/docs/components/DocsHeader/DocsHeader.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+import classes from './docsheader.module.css';
+
+export const DocsHeader: FC = () => <header className={classes.docsHeader}>Yeehaw Components Docs</header>;
--- src/docs/layouts/DocsLayout/DocsLayout.tsx
+++ src/docs/layouts/DocsLayout/DocsLayout.tsx
@@ -1,9 +1,11 @@
 import { FC, ReactNode } from 'react';
 
+import { DocsHeader } from '../../components';
 import classes from './docslayout.module.css';
 
 export const DocsLayout: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => (
   <main className={classes.docsLayout} id='docs-layout'>
-    {children}
+    <DocsHeader />
+    <article>{children}</article>
   </main>
 );
